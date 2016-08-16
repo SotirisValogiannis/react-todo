7a7db2eca46cbde37fac0403255a0a121e8a4c3f
Sotiris Valogiannis <sotirisvalogiannis@macbooks-MacBook-Pro.local>
2016-08-16 20:07:31 +0300

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,5 +18,33 @@ module.exports = {
     } catch (e) {}
 
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function(todos, showCompleted, searchText){
+    var filteredTodos = todos;
+
+    // Filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo)=> {
+      return !todo.completed || showCompleted;
+    })
+
+    // Filter by searchText
+    filteredTodos = filteredTodos.filter((todo)=> {
+      if( searchText === '' || todo.text.toLowerCase().indexOf(searchText) !== -1 ) {
+        return todo;
+      }
+    });
+
+    // Sort todos with non-completed first
+    filteredTodos.sort((a, b)=> {
+      if(!a.completed && b.completed ) {
+        return -1;
+      } else if( a.completed && !b.completed ) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    return filteredTodos;
   }
 }
