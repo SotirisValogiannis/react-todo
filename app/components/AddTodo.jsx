36ff5868d41d971bc013198f596f148ee29d415c
Sotiris Valogiannis <sotirisvalogiannis@macbooks-MacBook-Pro.local>
2016-08-28 21:31:18 +0300

    Refactor TodoList, Todo and AddTodo to work with redux

--- app/components/AddTodo.jsx
+++ app/components/AddTodo.jsx
@@ -1,20 +1,20 @@
 var React = require('react');
+var {connect} = require('react-redux');
+var actions = require('actions');
 
-var AddTodo = React.createClass({
+export var AddTodo = React.createClass({
   PropTypes: {
     onNewTodo: React.PropTypes.func.required
   },
   handleSubmit: function(e){
     e.preventDefault();
-
+    var dispatch = this.props.dispatch;
     var todoText = this.refs.todoText.value;
 
     if( todoText.length > 0) {
 
       this.refs.todoText.value = '';
-
-      // pass the value on onNewTodo() and make it visible on TodoApp.jsx
-      this.props.onAddTodo( todoText );
+      dispatch(actions.addTodo( todoText ));
     } else {
       this.refs.todoText.focus();
     }
@@ -31,4 +31,4 @@ var AddTodo = React.createClass({
   }
 });
 
-module.exports = AddTodo;
+export default connect()(AddTodo);
